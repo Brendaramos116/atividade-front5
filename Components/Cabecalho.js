5dcdfb3522f4b058350cb65bf74bb25b4f6fd7f8
unknown <brendaramos116@hotmail.com>
2023-04-19 20:17:04 -0300

    Second commit

---
+++ Components/Cabecalho.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { Container, Nav, Navbar } from 'react-bootstrap'
+
+const Cabecalho = () => {
+  return (
+    <>
+      <Navbar bg="dark" variant="dark">
+        <Container>
+          <Navbar.Brand href="#home">Sistema Acadêmico</Navbar.Brand>
+          <Nav className="me-auto">
+            <Nav.Link href="#home">Página 1</Nav.Link>
+            <Nav.Link href="#features">Página 2</Nav.Link>
+            <Nav.Link href="#pricing">Array</Nav.Link>            
+            <Nav.Link href="#pricing">Carros</Nav.Link>            
+          </Nav>
+        </Container>
+      </Navbar>
+
+    </>
+
+  )
+}
+
+export default Cabecalho
